992e42afb8b08054ee941e90c2c3d6643d979cf0
NightCoderMMCoder <laravelmastermmcoder@gmail.com>
2021-07-07 17:10:11 +0630

    14-NextTick

--- src/components/Chat/Right/MessagesList.vue
+++ src/components/Chat/Right/MessagesList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="chats-list">
+  <div class="chats-list" ref="chatsList">
     <MessageItem
       v-for="message in messages"
       :key="message.id"
@@ -9,10 +9,29 @@
 </template>
 
 <script>
+import { nextTick, ref, watch } from "vue";
 import MessageItem from "./MessageItem.vue";
 export default {
   components: { MessageItem },
   props: { messages: Array },
+  setup(props) {
+    const chatsList = ref(null);
+
+    watch(
+      () => props.messages,
+      () => {
+        nextTick(() => {
+          // chatsList.value.scrollTo(0, chatsList.value.scrollHeight);
+          chatsList.value.scroll({
+            top: chatsList.value.scrollHeight,
+            behavior: "smooth",
+          });
+        });
+      }
+    );
+
+    return { chatsList };
+  },
 };
 </script>
 
